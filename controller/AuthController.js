ea3330fe8cf28a547d0fd14213d8b3d699d64de3
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-09-02 22:17:50 +0600

    update

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,4 +1,5 @@
 import UserModel from '../Models/userModel.js';
+import bcrypt from 'bcrypt';
 
 //Registering a new user
 export const registerUser = async (req, res) => {
@@ -14,6 +15,26 @@ export const registerUser = async (req, res) => {
     }
 };
 // login User
+
+export const loginUser = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await UserModel.findOne({ username: username });
+
+        if (user) {
+            const validity = await bcrypt.compare(password, user.password);
+
+            validity
+                ? res.status(200).json(user)
+                : res.status(400).json('Wrong Password');
+        } else {
+            res.status(404).json('User does not exists');
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 export const loginUser = async (req, res) => {
     const { username, password } = req.body;
