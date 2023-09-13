20d68fc98ebc77d57ccb251598d64b53ab62a867
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-09-13 10:47:31 +0600

    user found added

--- controller/UserController.js
+++ controller/UserController.js
@@ -5,12 +5,16 @@ export const getUser = async (req, res) => {
     const id = req.params.id;
 
     try {
-        const user = await UserModel.findByID(id);
+        const user = await UserModel.findById(id);
         if (user) {
-            res.status(200).json(user);
+            const { password, ...otherDetails } = user._doc;
+            res.status(200).json(otherDetails);
             console.log('User data found');
+        } else {
+            res.status(404).json('No User Found');
         }
     } catch (error) {
         res.status(500).json(error);
+        console.log(error);
     }
 };
