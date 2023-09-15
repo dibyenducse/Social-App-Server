65eed437c0faf492c6bb788b3fb4cc0012d128ca
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-09-16 00:18:20 +0600

    update post

--- controller/PostController.js
+++ controller/PostController.js
@@ -45,3 +45,23 @@ export const updatePost = async (req, res) => {
         res.status(500).json(error);
     }
 };
+
+//Delete a post
+
+export const deletePost = async (req, res) => {
+    const id = req.params.id;
+    const { userId } = req.body;
+
+    try {
+        const post = await PostModel.findById(id);
+        if (post.userId === userId) {
+            await post.deleteOne(
+                res.status(200).json('Post Deleted Successfully')
+            );
+        } else {
+            res.status(403).json('Action Forbidden');
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -1,8 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { createPost, getPost } from '../controller/PostController.js';
+import {
+    createPost,
+    getPost,
+    updatePost,
+} from '../controller/PostController.js';
 
 router.post('/', createPost);
 router.get('/:id', getPost);
+router.put('/:id', updatePost);
 
 export default router;
